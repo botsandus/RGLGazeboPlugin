dbd4fda1b49bd96bbcbf967d1281eb98fb12a61a
Abhijith (AJ) Sampathkrishna <abhijith@dexory.com>
2023-05-09 13:32:49 +0100

    AUTO-803 helper update

--- plugin_setup.sh
+++ plugin_setup.sh
@@ -1,3 +1,4 @@
+#run this script with sudo
 cp install/RGLServerPlugin/libRobotecGPULidar.so /usr/lib/x86_64-linux-gnu/ign-gazebo-6/plugins/
 cp install/RGLServerPlugin/libRGLServerPluginInstance.so /usr/lib/x86_64-linux-gnu/ign-gazebo-6/plugins/
 cp install/RGLServerPlugin/libRGLServerPluginManager.so /usr/lib/x86_64-linux-gnu/ign-gazebo-6/plugins/
