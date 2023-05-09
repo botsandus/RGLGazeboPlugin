ee6b9baf0a9f4f3dc8e29cf6cfdc6ef5c8c17221
Abhijith (AJ) Sampathkrishna <abhijith@dexory.com>
2023-05-09 12:12:10 +0100

    AUTO-803 install helper

---
+++ plugin_setup.sh
@@ -0,0 +1,4 @@
+cp install/RGLServerPlugin/libRobotecGPULidar.so /usr/lib/x86_64-linux-gnu/ign-gazebo-6/plugins/
+cp install/RGLServerPlugin/libRGLServerPluginInstance.so /usr/lib/x86_64-linux-gnu/ign-gazebo-6/plugins/
+cp install/RGLServerPlugin/libRGLServerPluginManager.so /usr/lib/x86_64-linux-gnu/ign-gazebo-6/plugins/
+cp install/RGLVisualize/libRGLVisualize.so /usr/lib/x86_64-linux-gnu/ign-gazebo-6/plugins/gui/
